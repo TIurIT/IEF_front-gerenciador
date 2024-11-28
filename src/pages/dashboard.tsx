97e201c1fd28e65b3fef4f32af0baf4edca07903
iuriferrari <iuriestevam@hotmail.com>
2024-11-27 21:39:41 -0300

    Refresh att+ on going signin

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,22 @@
+import { Menubar,MenubarContent,MenubarItem,MenubarMenu,MenubarSeparator,MenubarShortcut,MenubarTrigger, } from "@/components/ui/menubar"
+
+
 function Dashboard() {
+    <Menubar>
+    <MenubarMenu>
+        <MenubarTrigger>File</MenubarTrigger>
+        <MenubarContent>
+            <MenubarItem>
+                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>New Window</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Share</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Print</MenubarItem>
+        </MenubarContent>
+    </MenubarMenu>
+</Menubar>
     return <h1>Dashboard</h1>
 }
 
--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,10 +1,36 @@
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {useForm} from "react-hook-form"
 
 function SignIn() {
+    const {register, handleSubmit} = useForm();
+
+    function handleSignin(data: any) {
+        console.log(data);
+    }
+
     return (
         <>
-            <h1>SignIn</h1>
-            <Button>Logar</Button>
+        <div className="flex flex-col justify-center w-[350px] gap-4">
+            <div className="flex flex-col text-center">
+                <h1 className="text-xl font-semibold" >
+                    Faça o seu login
+                </h1>
+            </div>
+            <form className="space-y-4" onSubmit={handleSubmit(handleSignin)}>
+                <div className="space-y-2">
+                <Label htmlFor="email">Digite o seu email</Label>
+                    <Input type="email" {...register('email')} />
+                </div>
+                <div className="space-y-2">
+                <Label htmlFor="password">Digite a sua senha</Label>
+                    <Input type="password" {...register('password')}/>
+                </div>
+            
+                <Button className="w-full">Logar</Button>
+            </form>
+        </div>
         </>
     )
 }
